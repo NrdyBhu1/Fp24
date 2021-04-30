4c4bdeee26b9e56ac164060082b76dac8bca239a
NrdyBhu1 <NrdyBhu1@gmail.com>
2021-04-30 12:39:19 +0530

    Fixed Black background 	modified:   scripts/utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,6 +1,7 @@
 import pygame
 
 def crop_img(sprite_sheet, x, y, w, h, scale_ratio):
-    surf = pygame.Surface((w, h)).convert_alpha()
+    surf = pygame.Surface((w, h))
     surf.blit(sprite_sheet.convert_alpha(), (x, y, w, h))
+    surf.set_colorkey((0, 0, 0))
     return pygame.transform.scale(surf, scale_ratio)
