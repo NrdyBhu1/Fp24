301d7f79ecad34461890e3aeba867af53cb306a8
NrdyBhu1 <NrdyBhu1@gmail.com>
2021-04-30 12:26:15 +0530

    Modified to fix black background 	modified:   scripts/coin.py 	modified:   scripts/utils.py 	modified:   settings.json

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,6 +1,6 @@
 import pygame
 
 def crop_img(sprite_sheet, x, y, w, h, scale_ratio):
-    surf = pygame.Surface((w, h))
-    surf.blit(sprite_sheet, (x, y, w, h))
+    surf = pygame.Surface((w, h)).convert_alpha()
+    surf.blit(sprite_sheet.convert_alpha(), (x, y, w, h))
     return pygame.transform.scale(surf, scale_ratio)
